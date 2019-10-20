ab25a4045b52ba9b980d75073ab92525f9661708
Johan Vandegriff <johan@vandymail.com>
2019-10-20 01:14:17 -0400

    changed tags format and removed more useless words

--- web_scrape/web_scrape.py
+++ web_scrape/web_scrape.py
@@ -49,6 +49,7 @@ def log_error(e):
 
 #output_file = sys.argv[1]
 output_file = "clubs.json"
+all_words_freq_file = "tags_freq.json"
 all_words_file = "tags.json"
 
 #url = "https://mason360.gmu.edu/club_signup\?view\=all\&"
@@ -59,8 +60,8 @@ raw_html = simple_get(url)
 html = BeautifulSoup(raw_html, 'html.parser')
 
 clubs = []
-all_words = {}
-words_to_ignore = ['and', 'the', 'to', 'of', 'in', 'a', 'is', 'for', 'mason', 'we', 'our', 'with', 'as', 'at', 'that', 'on', 'through', 'are', 'club', 'an', 'will', 'by', 'gmu', 'be', 'their', 'all', 'this', 'who', 'about', 'well', 'or', 'from', 'its', 'also', 'have', 'within', 'while', 'more', 'them', 'those', 'among', 'each', 'it', 'which', 'you', 'not', 's', 'can', 'want', 'out', 'but', 'they', 'non', 'both', 'any', 'into', 'such', 'where', 'every', 'has', 'come', 'these', 'was', 'get', 'become', 'like', 'amongst', 'us', 'take', 'so', 'most', 'e', '1', 'able', 'here', 'how', '3', '2', 'his', 'do', 'use', 'd', '4', 'no', 'c', 'if', '5', 'etc', 'u', 'eta', 'i', 'f', '6', '7', 'whose', 'https', 'my', 'r', 'k', 'put', 'yet', 'upon', "it's", 'end', 'ever', 'students', 'student', 'george', 'organization', 'university', 'mission', 'campus', 'members', 'association', 'provide', 'society', 'purpose', 'other']
+all_words_freq = {}
+words_to_ignore = ['and', 'the', 'to', 'of', 'in', 'a', 'is', 'for', 'mason', 'we', 'our', 'with', 'as', 'at', 'that', 'on', 'through', 'are', 'club', 'an', 'will', 'by', 'gmu', 'be', 'their', 'all', 'this', 'who', 'about', 'well', 'or', 'from', 'its', 'also', 'have', 'within', 'while', 'more', 'them', 'those', 'among', 'each', 'it', 'which', 'you', 'not', 's', 'can', 'want', 'out', 'but', 'they', 'non', 'both', 'any', 'into', 'such', 'where', 'every', 'has', 'come', 'these', 'was', 'get', 'become', 'like', 'amongst', 'us', 'take', 'so', 'most', 'e', '1', 'able', 'here', 'how', '3', '2', 'his', 'do', 'use', 'd', '4', 'no', 'c', 'if', '5', 'etc', 'u', 'eta', 'i', 'f', '6', '7', 'whose', 'https', 'my', 'r', 'k', 'put', 'yet', 'upon', "it's", 'end', 'ever', 'students', 'student', 'george', 'organization', 'university', 'mission', 'campus', 'members', 'association', 'provide', 'society', 'purpose', 'other', "'bid'", "'voices'", '000', '00pm', '10', '101', '15', '16', '18', '19', '1920s', '1931founding', '1957', '1960s', '1980s', '1982', '1987', '1995', '1998', '1st', '20', '2002', '2006', '2007', '2011', '2012', '2013', '2014', '2015', '2017', '2018', "21's", '21st', '230', '23rd', '26', '26th', '28', '300', '30pm', '30s', '3129', '31st', '35', '3rd', '40', '40k', '4220', '45', '450g', '4erxrqg', '4th', '501', '600', '6pm', '700', '703', '70s', '7pm', '800', '8pm', '90', '900', '90s', '993', '9pm']
 
 def add_words(text):
     text = text.replace('\u00c1', '')
@@ -68,10 +69,10 @@ def add_words(text):
     for word in re.findall(r"[\w']+", text):
         word = word.lower()
         if not len(word) < 2 and not word in words_to_ignore:
-            if word in all_words:
-                all_words[word] += 1
+            if word in all_words_freq:
+                all_words_freq[word] += 1
             else:
-                all_words[word] = 1
+                all_words_freq[word] = 1
 
 print("Extracting group titles and website links...")
 for a in html.select('li > div > div > div > div > h4 > a'):
@@ -126,9 +127,14 @@ print("Saving output to files...")
 with open(output_file, 'w') as f:
     json.dump(clubs, f, indent=4)
 
+with open(all_words_freq_file, 'w') as f:
+    json.dump(all_words_freq, f, indent=4)
+
+def custom_key(item):
+    return item['freq']
+
+all_words = [{'word':key, 'freq':all_words_freq[key]} for key in all_words_freq]
+all_words = sorted(all_words, key=custom_key, reverse=True)
+
 with open(all_words_file, 'w') as f:
     json.dump(all_words, f, indent=4)
-
-#sorted_words = sorted(all_words.items(), key=operator.itemgetter(1))
-#for word in sorted_words:
-#    print(word)
